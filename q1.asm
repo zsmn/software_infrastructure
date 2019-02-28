8a062b0ffb004b5eb943ad519e7696b7d26a67cf
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2019-02-28 00:31:55 -0300

    Update q1.asm

--- q1.asm
+++ q1.asm
@@ -1,15 +1,14 @@
 org 0x7c00
 jmp 0x0000:start
 
-res_col: resw 0
-res_line: resw 0
-
 start:
+
 	call reset_registers
 	call iniciar_video
 	jmp draw_outer_loop
 
 reset_registers:
+
 	xor ax, ax
 	mov ds, ax
 	mov cx, ax
@@ -18,6 +17,7 @@ reset_registers:
 	ret
 	
 iniciar_video:
+
 	mov ah, 4FH
 	mov al, 02H
 	mov bx, 10H
@@ -26,32 +26,50 @@ iniciar_video:
 	ret
 
 draw_outer_loop:
-	cmp word [res_line], 15 ; se fizer as 16 linhas (considera começando de 0)
+	cmp cx, 16 ; se fizer as 16 linhas (considera começando de 0)
 	je end ; se for igual vai pra end
 
-	add word [res_line], 1 ; adiciona +1 a res_line
+	add cx, 1 ; adiciona +1 a res_line
 
 draw_inner_loop:
-	cmp word [res_col], 15 ; se fizer as 16 colunas (considera começando de 0)
+
+	cmp dx, 16 ; se fizer as 16 colunas (considera começando de 0)
 	je reset_inner_loop ; se for igual jumpa pra reset
 
-	add word [res_col], 1 ; adiciona +1 a res_col
+	add dx, 1 ; adiciona +1 a res_col
 
-	mov ah, 0CH ; write pixel
-	mov al, 8 ; color
-	mov cx, word [res_col]; coluna
-	mov dx, word [res_line] ; linha
-	int 10h
+	; ideia: fazer dois 'prints' de pixel especificos
+	; onde um deles vai printar caso a asc2 esteja entre '0' e '9'
+	; e outro quando estiver entre 'A' e 'F'
+	; (fazer codigo em c++ pra a conversão!)
+	cmp dx, 8
+	ja print_pixel_aschigher
+
+	call print_pixel_asclower
 
 	jmp draw_inner_loop ; fica chamando no loop
 
+print_pixel_asclower:
+	mov ah, 0CH ; write pixel
+	mov al, 8 ; color
+	int 10h
+
+	ret
+
+print_pixel_aschigher:
+	mov ah, 0CH ; write pixel
+	mov al, 10 ; color
+	int 10h
+
+	jmp draw_inner_loop
+
 reset_inner_loop:
-	mov word [res_col], 0 ; reseta res_col pra 0
+
+	mov dx, 0 ; reseta res_col pra 0
 	jmp draw_outer_loop ; chama o outer loop
 
 end:
 	jmp $
-
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
