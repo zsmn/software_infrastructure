bb1bdc6bd63a6057ef6c38a1e30951cd4c7e726a
Zilde Neto <37539515+zsmn@users.noreply.github.com>
2019-03-08 12:24:01 -0300

    Update fake.asm

--- fake.asm
+++ fake.asm
@@ -1,10 +1,24 @@
-;;so ta faltando fazer os centavos
+;; falta consertar os cents
 
 org 0x7c00
 jmp 0x0000:start
 
 aux dw 0
-aux2 dw 0
+
+flag db 0
+
+qt_100 db 0
+qt_50 db 0
+qt_20 db 0
+qt_10 db 0
+qt_5 db 0
+qt_2 db 0
+qt_1 db 0
+qt_0_50 db 0
+qt_0_25 db 0
+qt_0_10 db 0
+qt_0_05 db 0
+qt_0_01 db 0
 
 putchar:
     mov ah, 0eh
@@ -36,8 +50,10 @@ start:
 
 exec:
     call getchar
+    cmp al, 44 ; ','
+    je .extra
     cmp al, 0x0d ; enter
-    je generate
+    je .troll
 
     inc cl
     call putchar
@@ -45,8 +61,14 @@ exec:
 
     jmp exec
 
+    .extra:
+        call putchar
+        jmp generate
+
+    .troll:
+        jmp generate
+
 generate:
-    call endl
     lodsb
     sub al, 48
     mov dh, 0
@@ -54,14 +76,14 @@ generate:
 
     dec cl
     cmp cl, 0
-    je calc_dol
+    je .select
 
     .every:
         xor ax, ax
         mov bx, 10
         
         cmp cl, 0
-        je calc_dol
+        je .select
 
         mov ax, dx
         mul bx
@@ -72,9 +94,14 @@ generate:
         add dx, ax
         dec cl
         jmp .every
+    
+    .select:
+        cmp byte[flag], 0
+        je calc_dol
+        cmp byte[flag], 1
+        je calc_cent
 
 esquemas:
-    call endl
     push dx
     call reset_register
     pop dx
@@ -85,68 +112,152 @@ esquemas:
 
     ret
 
-printar_coisa:
-    add ax, '0'
-    call putchar
-    xor ah, ah
-    sub ax, '0'
-    ret
-
 eliminar_dif:
     mul cx
     sub word[aux], ax
     mov dx, word[aux]
     ret
 
+div_add:
+    div cx
+    add ax, '0'
+    ret
+
+xor_sub:
+    xor ah, ah
+    sub ax, '0'
+    ret
+
 calc_dol:
     call esquemas
     mov cx, 100
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_100], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 50
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_50], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 20
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_20], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 10
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_10], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 5
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_5], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 2
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_2], al
+    call xor_sub
     call eliminar_dif
 
     call esquemas
     mov cx, 1
-    div cx
-    call printar_coisa
+    call div_add
+    mov byte[qt_1], al
+    call xor_sub
     call eliminar_dif
 
-    jmp end
+    mov byte[flag], 1
 
-debug:
-    call reset_register
-    mov al, 'T'
+    jmp exec
+
+calc_cent:
+
+    call esquemas
+    mov cx, 50
+    call div_add
+    mov byte[qt_0_50], al
+    call xor_sub
+    call eliminar_dif
+
+    call esquemas
+    mov cx, 25
+    call div_add
+    mov byte[qt_0_25], al
+    call xor_sub
+    call eliminar_dif
+
+    call esquemas
+    mov cx, 10
+    call div_add
+    mov byte[qt_0_10], al
+    call xor_sub
+    call eliminar_dif
+
+    call esquemas
+    mov cx, 5
+    call div_add
+    mov byte[qt_0_05], al
+    call xor_sub
+    call eliminar_dif
+
+    call esquemas
+    mov cx, 1
+    call div_add
+    mov byte[qt_0_01], al
+    call xor_sub
+    call eliminar_dif
+
+    jmp printarporratoda
+
+ajuda:
     call putchar
+    call endl
+    xor ax, ax
+    ret
+
+printarporratoda:
+    call endl
+    mov al, byte[qt_100]
+    call ajuda
+    mov al, byte[qt_50]
+    call ajuda
+    mov al, byte[qt_20]
+    call ajuda
+    mov al, byte[qt_10]
+    call ajuda
+    mov al, byte[qt_5]
+    call ajuda
+    mov al, byte[qt_2]
+    call ajuda
+    mov al, byte[qt_1]
+    call ajuda
+    mov al, byte[qt_0_50]
+    call ajuda
+    mov al, byte[qt_0_25]
+    call ajuda
+    mov al, byte[qt_0_10]
+    call ajuda
+    mov al, byte[qt_0_05]
+    call ajuda
+    mov al, byte[qt_0_01]
+    call putchar
+
+tests:
+    mov al, 'k'
+    call putchar
+    jmp end
 
 end:
     jmp $
